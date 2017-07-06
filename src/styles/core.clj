33282fcfae71a16b3a56554544962ca5e6ab35a6
Vlad Lopalo <vlad.lopalo@gmail.com>
2017-07-06 23:11:27 +0300

    compile training style using Garden

---
+++ src/styles/core.clj
@@ -0,0 +1,51 @@
+(ns styles.core
+  (:require [garden.def :refer [defstyles]]
+            [garden.units :refer [px em]]))
+
+
+(def type-selector
+  [:.type-selector {:position "absolute !important"
+                    :left (px 16)
+                    :width "9em !important"
+                    :bottom 0
+                    :text-align 'initial}])
+
+
+(def speech
+  [:.speech {:margin-left (em 1)
+             :position 'relative
+             :bottom (px -10)}])
+
+(def flashcard
+  [:.flashcard
+   {:padding (em 1)
+    :max-width (px 800)
+    :margin 'auto}
+   [:.card-text
+    {:margin-bottom (em 1)
+     :font-size (px 20)}
+    [:button
+     {:min-width "0 !important"}
+     [:span
+      {:padding-left "0.5em !important"
+       :padding-right "0.5em !important"}]]]])
+
+
+(def constructor
+  [:.constructor
+   {:padding (em 1)}
+   [:.source-text {:font-size (px 18)}]
+   [:.blocks
+    {:display 'flex
+     :flex-wrap 'wrap
+     :justify-content 'center
+     :margin (em 1)}
+    [:div {:margin "2px !important"}]]])
+
+(defstyles training
+  [:.training
+   {:text-align 'center}
+   type-selector
+   speech
+   flashcard
+   constructor])
